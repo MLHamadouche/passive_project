a0d5385f5dfd72ef1d205d7b2268bcdca43cd4e4
MLHamadouche <mham@roe.ac.uk>
2020-07-29 15:36:25 +0100

    plot and catalogue

--- stacks.py
+++ stacks.py
@@ -22,7 +22,7 @@ ID_list = df.set_index(passive_cut['FIELD'].str.decode("utf-8").str.rstrip() + p
 redshifts = passive_cut['z_spec']
 objects = np.array(passive_cut['FIELD'].str.decode("utf-8").str.rstrip() + passive_cut['ID_1'].astype(str).str.pad(6, side='left', fillchar='0')+ passive_cut['CAT'].str.decode("utf-8"))
 
-new_wavs = np.arange(2400, 4200,5) #new wavelength grid
+new_wavs = np.arange(2400, 4200,5.) #new wavelength grid
 
 new_spec = np.zeros(len(new_wavs))
 new_errs = np.zeros(len(new_wavs))
@@ -39,12 +39,11 @@ for ID in new_objs:
     spectrum=ld.load_vandels_spectra(ID)
     rest_wavs = spectrum[:,0]/(1.+z)
     mask = (rest_wavs < 3500) & (rest_wavs > 3000)
-    old_spec = spectrum[:,1]#/np.max(spectrum[:,1][mask]) #normalisation ????
-    old_errs = spectrum[:,2]#/np.max(spectrum[:,1][mask])
+    old_spec = spectrum[:,1]/np.max(spectrum[:,1][mask]) #normalisation ????
+    old_errs = spectrum[:,2]#/np.mean(spectrum[:,1][mask])
 
     new_spec, new_errs = spectres.spectres(new_wavs, rest_wavs, old_spec, spec_errs=old_errs)
-#plt.plot(new_wavs, new_spec)
-#plt.show()
+
 specc = np.zeros((len(new_wavs),len(new_objs)))
 specerrs = np.zeros((len(new_wavs),len(new_objs)))
 
@@ -54,6 +53,7 @@ for n in range(len(new_objs)):
 
 #print(specc, specerrs)
 print(specc.shape)
+print(specc[0:10])
 
 median_spec = np.zeros(len(new_wavs))
 errs = np.zeros(len(new_wavs))
@@ -64,22 +64,22 @@ for m in range(len(new_wavs)):
     spec_ = specc[m,:]
     spec_errs = specerrs[m,:]
     median_spec[m] = np.median(spec_)
-    #median_spec[m]  = np.nanmedian(spec_)
-    #errs[m] = np.median(spec_errs) defo no
+    #errs[m] = np.median(spec_errs) #nope
     #errs[m] = np.sqrt(1/np.sum(spec_errs**2)) #??? weighted errors
 
 print(median_spec.shape)
 print(new_wavs.shape)
 
 fig, (ax1, ax2) = plt.subplots(2, figsize=(12,7))
-grid = gridspec.GridSpec(2, 1, height_ratios=[3,1])
+grid = gridspec.GridSpec(2, 1, height_ratios=[4,1])
 ax1 = plt.subplot(grid[0])
-ax2 = plt.subplot(grid[1])
-ax1.plot(new_wavs, median_spec*10**18, color="black", lw=1.5, )
-#ax2.plot(new_wavs, errs*10**18, color="red", lw=1.5)
-ax1.set_ylabel("Flux $( \mathrm{W/m^2/\\AA)}$", size=15)
-ax2.set_ylabel("Flux Error", size=15)
-ax2.set_xlabel("Wavelength ($\mathrm{\AA}$)", size=15)
+#ax2 = plt.subplot(grid[1])
+ax1.plot(new_wavs, median_spec, color="black", lw=1.5, label = '5 $\mathrm{\AA}\ $ Sampling')
+#ax2.plot(new_wavs, errs, color="red", lw=1.5)
+ax1.set_ylabel("Flux $(\mathrm{W/m^2/\\AA)}$", size=15)
+#ax2.set_ylabel("Flux Error", size=15)
+ax1.set_xlabel("Wavelength ($\mathrm{\AA}$)", size=15)
 ax1.set_xlim(2300, 4300)
-ax2.set_xlim(2300, 4300)
-plt.show()
+#ax2.set_xlim(2300, 4300)
+ax1.legend()
+plt.savefig('stack_test_plot.pdf')

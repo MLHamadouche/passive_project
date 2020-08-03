200fd2cd5e90059ea6dfdfe328436f13bad61406
MLHamadouche <mham@roe.ac.uk>
2020-08-03 16:37:38 +0100

    sorted units and normalisation

--- stacks.py
+++ stacks.py
@@ -37,13 +37,15 @@ specc = np.zeros((len(new_wavs),len(new_objs)))
 specerrs = np.zeros((len(new_wavs),len(new_objs)))
 spec = []
 spec_err = []
+med_norm = []
 for ID in new_objs:
     z = ID_list.loc[ID, 'z_spec']
     spectrum=ld.load_vandels_spectra(ID)
     rest_wavs = spectrum[:,0]/(1.+z)
-    mask = (rest_wavs < 3500) & (rest_wavs > 3000)
-    old_spec = spectrum[:,1]/np.median(spectrum[:,1][mask]) #normalisation ????
+    mask = (rest_wavs < 3500) & (rest_wavs > 3000) # fairly featureless region of spectrum
+    old_spec = spectrum[:,1]/np.median(spectrum[:,1][mask]) #normalisation median from that region
     old_errs = spectrum[:,2]/np.median(spectrum[:,2][mask])
+    med_norm.append(np.median(spectrum[:,1][mask]))
 
     new_spec, new_errs = spectres.spectres(new_wavs, rest_wavs, old_spec, spec_errs=old_errs)
     spec.append(new_spec)
@@ -51,14 +53,6 @@ for ID in new_objs:
 
 spec = np.transpose(spec)
 spec_err = np.transpose(spec_err)
-
-#for n in range(len(new_objs)):
-#    specc[:,n] = spec
-#    specerrs[:,n] = spec_err
-
-#print(specc, specerrs)
-#print(specc.shape)
-#print(specc[0:10])
 
 median_spec = np.zeros(len(new_wavs))
 errs = np.zeros(len(new_wavs))
@@ -72,17 +66,27 @@ for m in range(len(new_wavs)):
     errs[m] = np.median(spec_errs) #nopes
     #errs[m] = np.sqrt(1/np.sum(spec_errs**2)) #??? weighted errors
 
-
-fig, (ax1, ax2) = plt.subplots(2, figsize=(12,7))
-grid = gridspec.GridSpec(2, 1, height_ratios=[4,1])
+med_new = np.median(med_norm) # multiply stacked median spectrum by this value to get back the units for flux
+med_spec_units = median_spec*med_new
+"""
+fig, (ax1, ax2) = plt.subplots(2, figsize=(15,7))
+grid = gridspec.GridSpec(2, 1, height_ratios=[6,1])
 ax1 = plt.subplot(grid[0])
-ax2 = plt.subplot(grid[1])
-ax1.plot(new_wavs, median_spec, color="black", lw=1.5)#, label = '1.25 $\mathrm{\AA}\ $ Sampling')
-ax2.plot(new_wavs, errs, color="red", lw=1.5)
-ax1.set_ylabel("Flux (arbitrary units)", size=15) # $(\mathrm{W/m^2/\\AA)}$
-ax2.set_ylabel("Flux Error", size=15)
-ax2.set_xlabel("Wavelength ($\mathrm{\AA}$)", size=15)
+#ax2 = plt.subplot(grid[1])
+ax1.plot(new_wavs, med_spec_units*10**18, color="black", lw=1.5)#, label = '1.25 $\mathrm{\AA}\ $ Sampling')
+#ax2.plot(new_wavs, errs, color="red", lw=1.5)
+ax1.set_ylabel("Flux $(10^{-18}\ \mathrm{W/m^2/\\AA)}$", size=15) # $(\mathrm{W/m^2/\\AA)}$
+#ax2.set_ylabel("Flux Error", size=15)
+ax1.set_xlabel("Wavelength ($\mathrm{\AA}$)", size=15)
 ax1.set_xlim(2300, 4300)
-ax2.set_xlim(2300, 4300)
+#ax2.set_xlim(2300, 4300)
 ax1.set_title('Median Stacked Spectra of galaxies 1 < z < 1.5')
-plt.savefig('stack_test_plot_all.pdf')
+plt.savefig('stack_plot_all.pdf')
+"""
+plt.figure(figsize=(15,7))
+plt.plot(new_wavs, med_spec_units*10**18, color="black", lw=1.5 )
+plt.xlabel("Wavelength ($\mathrm{\AA}$)", size=15)
+plt.ylabel("Flux $(10^{-18}\ \mathrm{erg\ s^{-1}\ cm^{-2}\ \\AA{^-1})}$", size=15)
+plt.xlim(2300, 4250)
+plt.title('Median Stacked Spectra of galaxies 1 < z < 1.5', size=17)
+plt.savefig('stack_plot_all.pdf')

corrected hdelta index calculation

indices.py
@@ -5,7 +5,7 @@ import astropy.table as Table
 import LoadData as ld
 
 #input median stacked fluxes,
-wavs = np.arange(2400, 4200, 1.5)
+wavs = np.arange(2400, 4200, 1.25)
 
 def Dn4000(fluxes):
     #n is for narrow - less sensitive to reddening effects, used more in literature - 4000A to 4100A instead of 4050 - 4250
@@ -39,29 +39,34 @@ def C29_33(fluxes):
     return C_ind
 
 
-def H_delta(wavs, fluxes):
-    #from Bolagh.M, 1999
-    #changed to make either side 40Angstroms so no weighting change needed
-    mask_blue = (wavs > 4042) & (wavs<4082) #blue continuum
-
-    flux_blue = fluxes[mask_blue]
-    mask_red = (wavs > 4122) & (wavs<4162) #red continuum
-    flux_red = fluxes[mask_red]
+def H_delta(fluxes, wavs):
+    #continuum & line edges from Bolagh.M, 1999
+    #changed to make either side 40 Angstroms so no weighting change needed
+    blue_cont = [4042., 4082.]
+    red_cont = [4122., 4162.]
+    line_edges = [4082., 4122.]
+    mask_blue = (wavs > blue_cont[0]) & (wavs < blue_cont[1]) #blue continuum
+    flux_blue = np.array(fluxes[mask_blue])
+    mask_red = (wavs > red_cont[0]) & (wavs < red_cont[1]) #red continuum
+    flux_red = np.array(fluxes[mask_red])
     #flux_red = fluxes[mask_red]
-    line = (wavs>4082) & (wavs < 4122)
-    flux_line = fluxes[line]
-    flux_continuum = np.mean(np.mean(flux_blue)+np.mean(flux_red)) #one number
-    H_delta_EW = ((flux_continuum - np.mean(flux_line))*40)/flux_continuum
+    line = (wavs > line_edges[0]) & (wavs < line_edges[1])
+
+    flux_feature = np.nanmean(np.array(fluxes[line]))
+
+    line_width  = line_edges[1] - line_edges[0] #angstroms
+    flux_continuum = np.sum(np.nanmean(flux_blue)+np.nanmean(flux_red))/2 #one number
+    H_delta_EW = line_width*(flux_continuum - flux_feature)
+    H_delta_EW /=flux_continuum
     #multiply by 40 angstroms / continuum flux
     # EW found by summing fluxes in region, and fitting with gaussian
 
     return H_delta_EW #negative if emission, postive if absorption
-#mask_2825 = (wavs > 2725) & (wavs < 2825)
-#print(wavs[mask_2825])
+
 
 
 def Mg_UV(flux):
-    mask_2625 = (wavs<2625)& (wavs > 2525)
+    mask_2625 = (wavs < 2625)& (wavs > 2525)
     mask_2725 = (wavs > 2625) & (wavs < 2725)
     mask_2825 = (wavs > 2725) & (wavs < 2825)
     int_flux_2725 = np.trapz(flux[mask_2725], x = wavs[mask_2725])

stacking_code.py
@@ -35,7 +35,7 @@ ID_ = df.set_index(concat_3dhst['FIELD'].str.decode("utf-8").str.rstrip() + conc
 
 
 def stacks(objects_list): #input array of redshifts for given list of objects
-    new_wavs = np.arange(2400, 4200, 1.5)
+    new_wavs = np.arange(2400, 4200, 1.25)
     new_spec = np.zeros(len(new_wavs))
     new_errs = np.zeros(len(new_wavs))
 
@@ -90,7 +90,7 @@ def stacks(objects_list): #input array of redshifts for given list of objects
         D4000_index.append(Dn4000(new_spec))
         Mg_UV_index.append(Mg_UV(new_spec))
         H_delta_EW.append(H_delta(new_wavs,new_spec))
-
+        #print(len(H_delta_EW))
         spec.append(new_spec)
         spec_err.append(new_errs)
 
@@ -104,17 +104,17 @@ def stacks(objects_list): #input array of redshifts for given list of objects
     plt.rc('xtick', labelsize='x-small')
     plt.rc('ytick', labelsize='x-small')
     fig1, ax1 = plt.subplots(figsize=[12,8.5])
-    im1 = ax1.scatter(colour_index, Mg_UV_index, s=130, c=all_masses, cmap=plt.cm.magma, marker='o', edgecolors='black',linewidth=0.5 )
+    im1 = ax1.scatter(D4000_index, H_delta_EW, s=130, c=new_redshifts, cmap=plt.cm.magma, marker='o', edgecolors='black',linewidth=0.5 )
     cbar = fig1.colorbar(im1, ax=ax1)
-    cbar.set_label(r'log10(M*/Msun)', size=12)
+    cbar.set_label(r'Redshift, z', size=12)
     #ax1.scatter(colour_index, Mg_UV_index)
-    ax1.set_xlabel("C(29-33)", size=13)
-    ax1.set_ylabel("MgUV index", size=13)
-    plt.title('Colour index versus MgUV index', size =14)# excluding possible AGN (CDFS + UDS)')
-    #plt.savefig('C_2933vMgUV_allobjects_Mcbar.pdf')
+    ax1.set_xlabel("Dn4000)", size=13)
+    ax1.set_ylabel("EW(Hdelta)", size=13)
+    plt.title('EW(Hdelta) versus Dn4000', size =14)# excluding possible AGN (CDFS + UDS)')
+    #plt.savefig('EW(Hdelta)vDn4000.pdf')
     plt.close()
 
-    print(H_delta_EW)
+    #print(H_delta_EW)
 
     spec_err = np.transpose(spec_err)
     standev_err = []
@@ -125,9 +125,9 @@ def stacks(objects_list): #input array of redshifts for given list of objects
         #standev_err[m] = np.std(spec_, axis=0)
         median_spec[m]=np.nanmedian(spec_)
 
-    #med_spec_units = median_spec*med_new #test removing normalisations to see fluxes nh
+    med_spec_units = median_spec*med_new #test removing normalisations to see fluxes nh
 
-    return median_spec #returns an array of the new median stacked fluxes
+    return med_spec_units #returns an array of the new median stacked fluxes
 
 
 

stacks_M_Re.py
@@ -251,13 +251,8 @@ IDs_below= IDs[index_masked2].str.decode("utf-8").str.rstrip().values
 #print(IDs_above, IDs_below)
 all_IDs = np.concatenate((IDs_above, IDs_below), axis = None)
 
-#print(all_IDs)
+print(len(all_IDs))
 
-from indices import C29_33, Dn4000, Mg_UV # H_delta
-
-colour_index = []
-D4000_index =[]
-Mg_UV_index = []
 """"
 for names in all_IDs:
     spectrum = ld.load_vandels_spectra(names)
@@ -408,8 +403,8 @@ IDs_below_1075_11= IDs[index_masked2_mass].str.decode("utf-8").str.rstrip().valu
 #print(IDs_below_1075_11)
 
 #if y0 > 0.56*x0 + best_c:
-#stacking_above_1075_11 = stacks(IDs_above_1075_11)
-new_wavs = np.arange(2400, 4200, 1.5)
+
+new_wavs = np.arange(2400, 4200, 1.25)
 
 def plot_stackssingle(stack, name, color):
     plt.rc('font', family='serif')
@@ -429,8 +424,8 @@ def plot_stackssingle(stack, name, color):
 #plot_stackssingle(stacking_above_1075_11, 'above', 'r')
 
 
-
-#stacking_below_1075_11 = stacks(IDs_below_1075_11)
+stacking_above_1075_11 = stacks(IDs_above_1075_11)
+stacking_below_1075_11 = stacks(IDs_below_1075_11)
 
 #plot_stackssingle(stacking_below_1075_11,'below', 'k')
 
@@ -455,29 +450,29 @@ def plot_stacks(stack1, stack2):
 
 #plot_stacks(stacking_above_1075_11, stacking_below_1075_11)
 
-"""
+
 c_ind = C29_33(stacking_below_1075_11)
-print(f'C(29-33) index below: {c_ind}')
+print(f'C(29-33){low_lim} - {upp_lim} index below: {c_ind}')
 c_ind = C29_33(stacking_above_1075_11)
-print(f'C(29-33) index above : {c_ind}')
+print(f'C(29-33) {low_lim} - {upp_lim}  index above : {c_ind}')
 dn4000 = Dn4000(stacking_above_1075_11)
-print(f'Dn4000 index above: {dn4000}')
+print(f'Dn4000 {low_lim} - {upp_lim} index above: {dn4000}')
 dn4000 = Dn4000(stacking_below_1075_11)
-print(f'Dn4000 index below: {dn4000}')
+print(f'Dn4000 {low_lim} - {upp_lim} index below: {dn4000}')
 
 
-Mg_UV_index = Mg_UV(stacking_below)
-print(f'Mg_UV index below: {Mg_UV_index}')
+Mg_UV_index = Mg_UV(stacking_below_1075_11)
+print(f'Mg_UV {low_lim} - {upp_lim} index below: {Mg_UV_index}')
 #plot_stackssingle(stacking_below,'below', 'k')
-Mg_UV_index = Mg_UV(stacking_above)
-print(f'Mg_UV index above: {Mg_UV_index}')
+Mg_UV_index = Mg_UV(stacking_above_1075_11)
+print(f'Mg_UV {low_lim} - {upp_lim} index above: {Mg_UV_index}')
 #𝑦0>𝑎𝑥0+𝑏 then the point is above the line, etc.
-H_delta_EW = H_delta(stacking_above)
-print(f'H_delta_EW above: {H_delta_EW}')
+H_delta_EW = H_delta(stacking_above_1075_11, new_wavs)
+print(f'H_delta_EW {low_lim} - {upp_lim} above: {H_delta_EW}')
 
-H_delta_EW = H_delta(stacking_below)
-print(f'H_delta_EW above: {H_delta_EW}')
-"""
+H_delta_EW = H_delta(stacking_below_1075_11, new_wavs)
+print(f'H_delta_EW {low_lim} - {upp_lim} below: {H_delta_EW}')
+input()
 mask = (masses>10.4)
 df2 = pd.DataFrame(cat1[mask])
 R_e = df2['Re_kpc']